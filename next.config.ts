d924dd03a057e87b5ba0a7ab441e94a470e23828
Weinan Zhao <zhaown@rd.netease.com>
2026-02-11 19:22:42 +0800

    Fix cloudflare deploy error

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,10 @@ import createNextIntlPlugin from 'next-intl/plugin';
 const withNextIntl = createNextIntlPlugin();
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default withNextIntl(nextConfig);
